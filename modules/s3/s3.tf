21bc083268617cef9e2c963a87f72c529bc4d927
PassiveModding <github@jaquesy.me>
2022-08-26 10:21:00 +1000

    Update deprecated use of s3_bucket acl

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,7 +1,10 @@
 ### Create Resources
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket
-  acl    = "private"
-
   tags   = var.tags
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
 }
