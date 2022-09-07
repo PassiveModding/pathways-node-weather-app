71c07aec8c2edcf197edd0a76d4d267f094fd8a5
PassiveModding <github@jaquesy.me>
2022-09-07 15:37:50 +1000

    Allow force_delete on ecr

--- infra/tf/registry/main.tf
+++ infra/tf/registry/main.tf
@@ -1,5 +1,7 @@
 resource "aws_ecr_repository" "this" {
   name = var.repo_name
+  # ensures the repo is allowed to be destroyed by terraform even if it contains images
+  force_delete = true
 
   # mutable allows things like using a "LATEST" tag on the docker image.
   # ie. LATEST's image can change as it is updated. IMMUTABLE denies this behavior
