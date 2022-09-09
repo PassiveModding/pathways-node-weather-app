837ececa53af2af395ac5c1d1a6e62eecc7cb184
PassiveModding <github@jaquesy.me>
2022-09-09 17:10:59 +1000

    Split ecs tf into its own file & update tagging

--- infra/tf/application/main.tf
+++ infra/tf/application/main.tf
@@ -50,60 +50,6 @@ data "aws_subnets" "private" {
   }
 }
 
-resource "aws_ecs_cluster" "this" {
-  name = "${var.app_name}-cluster"
-
-  tags = var.tags
-}
-
-resource "aws_ecs_service" "this" {
-  name            = "${var.app_name}-ecs"
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.this.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    security_groups  = [aws_security_group.ecs.id]
-    subnets          = data.aws_subnets.private.ids
-    assign_public_ip = false
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.this.arn
-    container_name   = "${var.app_name}-container"
-    container_port   = var.container_port
-  }
-
-  tags = var.tags
-}
-
-resource "aws_ecs_task_definition" "this" {
-  network_mode             = "awsvpc"
-  family                   = "${var.app_name}-family"
-  requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.execution.arn
-  cpu                      = 256
-  memory                   = 512
-  container_definitions = jsonencode(
-    [
-      {
-        name      = "${var.app_name}-container",
-        image     = "${data.aws_ecr_repository.this.repository_url}:latest",
-        essential = true,
-        cpu       = 0,
-        portMappings = [
-          {
-            protocol      = "tcp",
-            containerPort = "${var.container_port}"
-          }
-        ]
-      }
-  ])
-
-  tags = var.tags
-}
-
 resource "aws_cloudwatch_log_group" "this" {
   name_prefix = var.resource_name_prefix
 }
